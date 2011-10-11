3cbdc16c376329bb54d24cd482ffef9e0a3667f7
awexomeguy <cljvf9@mail.mst.edu>
2011-10-10 19:40:29 -0500

    comments to capture thoughts

--- mynetwork.cpp
+++ mynetwork.cpp
@@ -3,5 +3,9 @@
 
 void MyNetwork::send()
 {
+  // just put sendto() here.
+  // eventually, we will make this "unreliable"
+  // eg only send based on a certain probability
+  
   //std::cout << "test send in network layer\n";
 }
